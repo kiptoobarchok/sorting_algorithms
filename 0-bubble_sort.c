8bdfb6dd00ba2a7437d2cfeee8122fe38708813b
kiptoobrachok <calebkiptoo9090@gmail.com>
2023-10-26 21:52:59 +0300

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,24 +10,20 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	int temp;
-
-	size_t i, j;
+	size_t j, i;
+	int n;
 
 	if (size < 2)
-	{
 		return;
-	}
-
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
+				n = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				array[j + 1] = n;
 				print_array(array, size);
 			}
 		}
