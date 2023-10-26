fe8869c70177ffc666f869a7c2f5cac0f9af779d
kiptoobrachok <calebkiptoo9090@gmail.com>
2023-10-26 21:47:02 +0300

    bubble sort

--- sort.h
+++ sort.h
@@ -34,9 +34,9 @@ void quick_sort_hoare(int *array, size_t size);
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 
