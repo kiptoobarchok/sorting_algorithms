9cb352a85f8552f1cde2286618293c662d04b92a
kiptoobrachok <calebkiptoo9090@gmail.com>
2023-10-27 14:15:05 +0300

    shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sort an array using shell_sort
+ * @array: array
+ * @size: size of array
+*/
+
+void shell_sort(int *array, size_t size)
+{
+    unsigned int gap = 1, i, j;
+    int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			j = i;
+
+
+			while (j >= gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+			array[j] = temp;
+		}
+		gap /= 3;
+		print_array(array, size);
+	}
+}
+
