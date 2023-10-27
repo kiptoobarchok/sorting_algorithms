d2567971845a424f17431270dbf2d0fc2c0ed728
kiptoobrachok <calebkiptoo9090@gmail.com>
2023-10-27 11:10:00 +0300

    buble

--- sort.h
+++ sort.h
@@ -6,6 +6,22 @@
 /*Libraries*/
 #include <stdio.h>
 
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+
+
 /*Function prototypes*/
 
 void print_array(const int *array, size_t size);
@@ -23,20 +39,6 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
-
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
-typedef struct listint_s
-{
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
-} listint_t;
 
 
 
