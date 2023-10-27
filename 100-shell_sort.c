b3edc4e4acb7096ee61a201ebfc108efa6c20b69
kiptoobrachok <calebkiptoo9090@gmail.com>
2023-10-27 14:17:36 +0300

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,15 +8,14 @@
 
 void shell_sort(int *array, size_t size)
 {
-    unsigned int gap = 1, i, j;
-    int temp;
+	unsigned int gap = 1, i, j;
+	int temp;
 
 	if (array == NULL || size < 2)
 		return;
 
 	while (gap < size / 3)
 		gap = gap * 3 + 1;
-
 	while (gap > 0)
 	{
 		for (i = gap; i < size; i++)
